54a3d109cc9dbc6da7f547a31824e81eb9da653b
host1812 <host1812@gmail.com>
2022-09-11 16:04:58 -0700

    simple authn

---
+++ gauthn/go.mod
@@ -0,0 +1,3 @@
+module github.com/host1812/gtutor-practical/gauthn
+
+go 1.19
---
+++ gauthn/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func usage() {
+	fmt.Println("usage:")
+	fmt.Println("./authn <user> <password>")
+}
+
+func main() {
+	uDb := map[string]string{
+		"alex": "pass",
+		"bob":  "1234",
+	}
+	// fmt.Printf("%v, %T", uDb, uDb)
+
+	if len(os.Args) < 3 {
+		fmt.Println("error: no username and/or password provided")
+		usage()
+		return
+	}
+
+	user := os.Args[1]
+	pass := os.Args[2]
+
+	if p, ok := uDb[user]; ok {
+		if pass == p {
+			fmt.Println("access granted")
+		} else {
+			fmt.Println("access denied")
+		}
+	} else {
+		fmt.Println("access denied")
+	}
+
+}
