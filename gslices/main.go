79d49b6e7a46c22e7201e429bd310c728d3b120e
host1812 <host1812@gmail.com>
2022-09-13 17:05:11 -0700

    playing wiht slices

--- gslices/main.go
+++ gslices/main.go
@@ -7,9 +7,15 @@ func main() {
 
 	fruits2 := []string{"banana2", "apple2"}
 
-	fruits = fruits2
-	fruits[0] = "banana0"
-
 	fmt.Println(fruits)
 	fmt.Println(fruits2)
+
+	ints := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(ints)
+	intsA := ints[1:3]
+	fmt.Println(intsA)
+	intsA[0] = 20
+	intsA[1] = 30
+	fmt.Println(intsA)
+	fmt.Println(ints)
 }
