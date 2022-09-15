1e3da4e858d9c3b1dde5716fd3dfb32c7b2c0e3f
host1812 <host1812@gmail.com>
2022-09-14 17:48:51 -0700

    v2 and v3 update bounce

--- gbounce/main.go
+++ gbounce/main.go
@@ -8,8 +8,13 @@ import (
 	"time"
 )
 
-const fieldSizeX = 25
-const fieldSizeY = 150
+const fieldSizeX = 15
+const fieldSizeY = 30
+
+type board struct {
+	field            [fieldSizeX][fieldSizeY]bool
+	x, y, dirx, diry int
+}
 
 func clear() {
 	cmd := exec.Command("clear")
@@ -55,17 +60,65 @@ func update(f *[fieldSizeX][fieldSizeY]bool, x, y, xdir, ydir int) (int, int, in
 	return x, y, xdir, ydir
 }
 
+func update2(f *[fieldSizeX][fieldSizeY]bool, x, y, xdir, ydir *int) {
+	f[*x][*y] = false
+	if *x > 0 && *x < fieldSizeX-1 {
+		*x += *xdir
+	} else {
+		*xdir = -*xdir
+		*x += *xdir
+	}
+
+	if *y > 0 && *y < fieldSizeY-1 {
+		*y += *ydir
+	} else {
+		*ydir = -*ydir
+		*y += *ydir
+	}
+
+	f[*x][*y] = true
+}
+
+func update3(b *board) {
+	b.field[b.x][b.y] = false
+	if b.x > 0 && b.x < fieldSizeX-1 {
+		b.x += b.dirx
+	} else {
+		b.dirx = -b.dirx
+		b.x += b.dirx
+	}
+
+	if b.y > 0 && b.y < fieldSizeY-1 {
+		b.y += b.diry
+	} else {
+		b.diry = -b.diry
+		b.y += b.diry
+	}
+
+	b.field[b.x][b.y] = true
+}
+
 func main() {
-	x, y := rand.Intn(fieldSizeX), rand.Intn(fieldSizeY)
-	xdir, ydir := 1, 1
-	field := [fieldSizeX][fieldSizeY]bool{}
-	field[x][y] = true
+	rand.Seed(time.Now().UnixMilli())
+	// xdir, ydir := 1, 1
+	// field := [fieldSizeX][fieldSizeY]bool{}
+	// field[x][y] = true
+	b := board{
+		field: [fieldSizeX][fieldSizeY]bool{},
+		dirx:  1,
+		diry:  1,
+		x:     rand.Intn(fieldSizeX),
+		y:     rand.Intn(fieldSizeY),
+	}
+	b.field[b.x][b.y] = true
 	for {
 		clear()
 
-		render(&field)
-		x, y, xdir, ydir = update(&field, x, y, xdir, ydir)
+		render(&b.field)
+		// x, y, xdir, ydir = update(&field, x, y, xdir, ydir)
+		// update2(&field, &x, &y, &xdir, &ydir)
+		update3(&b)
 
-		time.Sleep(20 * time.Millisecond)
+		time.Sleep(200 * time.Millisecond)
 	}
 }
