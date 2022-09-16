a68fd96a8ba8cef123a12963f743456be27ad46c
host1812 <host1812@gmail.com>
2022-09-16 07:22:04 -0700

    testing regex

--- glogscanner/main.go
+++ glogscanner/main.go
@@ -4,25 +4,54 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
-
-	"github.com/kylelemons/godebug/pretty"
 )
+
+func parse(s string) string {
+	if strings.ContainsAny(s, ",.;-:") {
+		replacer := strings.NewReplacer(
+			",", "",
+			".", "",
+			";", "",
+			":", "",
+		)
+		s = replacer.Replace(s)
+	}
+	return s
+}
+
+func parseRx(s string, r *regexp.Regexp) string {
+	matches := r.FindStringSubmatch(s)
+	if len(matches) > 0 {
+		return matches[0]
+	}
+	return ""
+}
+
+func parseRx2(s string, r *regexp.Regexp) string {
+	// fmt.Println("debug:", r.FindStringSubmatch(s))
+	return r.ReplaceAllString(s, "")
+}
 
 func main() {
 	in := bufio.NewScanner(os.Stdin)
 	in.Split(bufio.ScanWords)
 	words := map[string]int{}
+	r, err := regexp.Compile(`[^a-z]+`)
+	if err != nil {
+		fmt.Println("failed to compile regex")
+	}
 	for in.Scan() {
-		w := in.Text()
-		if strings.ContainsAny(w, ",.;-") {
-			replacer := strings.NewReplacer(",", "", ".", "", ";", "")
-			w = replacer.Replace(w)
+		// w := parse(in.Text())
+		w := parseRx(in.Text(), r)
+		if w != "" {
+			words[w] += 1
 		}
-		words[w] += 1
+
 	}
 	fmt.Println(words)
-	pretty.Print(words)
+	// pretty.Print(words)
 	// fmt.Println("scanned:", in.Text())
 	// file, err := os.Open("./sample")
 	// if err != nil {
