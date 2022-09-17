90b6ecbc6e28548563b512306bf8744c94ff7699
host1812 <host1812@gmail.com>
2022-09-17 12:45:54 -0700

    gjson: move all objects to submodule

--- gjson/main.go
+++ gjson/main.go
@@ -3,64 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+
+	o "github.com/host1812/gtutor-practical/gjson/objects"
 )
-
-const (
-	artifactPDF = iota
-	artifactWord
-	artifactExcel
-	artifactPowerPoint
-)
-
-const (
-	resultQBR = iota
-	resultMBR
-	resultYBR
-)
-
-type Artifact struct {
-	Name string
-	Type int
-	Size int
-	Tags
-}
-
-type Result struct {
-	Name      string
-	Type      int
-	Viewed    bool
-	Artifacts []Artifact
-	Tags
-	Authors
-	JustForFun `json:",omitempty"`
-}
-
-type Tags map[string]string
-type Authors []string
-type JustForFun []int
 
 func main() {
-	r1 := Result{
+	r1 := o.Result{
 		Name:   "Quarterly Business Review - Q2FY22",
-		Type:   resultQBR,
+		Type:   o.ResultQBR,
 		Viewed: false,
-		Artifacts: []Artifact{
+		Artifacts: []o.Artifact{
 			{
 				Name: "Presentation #1",
-				Type: artifactPowerPoint,
+				Type: o.ArtifactPowerPoint,
 				Size: 10031,
 				Tags: map[string]string{"cool": "maybe"},
 			},
 			{
 				Name: "Sales #1",
-				Type: artifactExcel,
+				Type: o.ArtifactExcel,
 				Size: 100310,
-				Tags: Tags{"confidential": "yes"},
+				Tags: o.Tags{"confidential": "yes"},
 			},
 		},
-		Tags:       Tags{"cool": "super"},
-		Authors:    Authors{"Alex", "Todd"},
-		JustForFun: JustForFun{1, 2, 3},
+		Tags:       o.Tags{"cool": "super"},
+		Authors:    o.Authors{"Alex", "Todd"},
+		JustForFun: o.JustForFun{1, 2, 3},
 	}
 
 	// fmt.Printf("r1: %#v\n", r1)
@@ -69,7 +37,7 @@ func main() {
 	r3 := r1
 	r3.Name = "Again"
 	r3.JustForFun = nil
-	results := []Result{r1, r2, r3}
+	results := []o.Result{r1, r2, r3}
 	// out, err := json.MarshalIndent(results, "", "\t")
 	out, err := json.Marshal(results)
 	if err != nil {
