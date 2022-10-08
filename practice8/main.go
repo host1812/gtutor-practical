a4162f1654813b82d8a6b2a7d86f72be3418a4df
host1812 <host1812@gmail.com>
2022-10-08 15:52:49 -0700

    dirwalk - initi

--- practice8/main.go
+++ practice8/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
 
+const offStr = "----"
+
+func readFile(dir string, offset int) {
+	if offset < 1 {
+		offset = 1
+	}
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		fmt.Printf("%s - doesn't exists\n", dir)
+		os.Exit(1)
+	}
+	dirEntries, _ := os.ReadDir(dir)
+	for _, e := range dirEntries {
+		if e.IsDir() {
+			fmt.Printf("%s %s (d)\n", strings.Repeat(offStr, offset), e.Name())
+			readFile(path.Join(dir, e.Name()), offset+1)
+		}
+		fmt.Printf("%s └ %s (f)\n", strings.Repeat(offStr, offset-1), e.Name())
+	}
+	// if fs.IsDir() {
+	// 	// fmt.Printf("%s - is not a directory\n", dir)
+	// 	fmt.Printf("%s- %s (d)", strings.Repeat(offStr, offset), fs.Name())
+	// 	readFile(fs.Name(), offset+1)
+	// }
+	// fmt.Printf("%s- %s (f)", strings.Repeat(offStr, offset), fs.Name())
+	// os.ReadDir(dir)
+}
+
+func main() {
+	readFile("./generated", 0)
 }
